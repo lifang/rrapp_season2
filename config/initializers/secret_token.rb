2bea59aa015d772f592ae77f071e6889a676d199
lifang <santhe_2004@126.com>
2012-07-19 09:53:09 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RrappSeason2::Application.config.secret_token = '89f9f6386d35c29c1298a83ede2d10a6cbc03f531e7361b230c363d868bf27ba58a93ee47a24455dc7620be0eb6365c90051206e063fb292aab9de86c0baa60c'
