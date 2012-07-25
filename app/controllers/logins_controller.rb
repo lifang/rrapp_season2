625cca3fb57127ecfed21942a3ae9dd8169dd952
qianjun <er6788@126.com>
2012-07-25 15:35:54 +0800

    qianjun

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -4,10 +4,11 @@ class LoginsController < ApplicationController
 
   def get_info
     session[:user_info]=renren_get_user(params[:access_token])
+    p session[:user_info]
     render :inline=>"response"
   end
 
   def get_callback
-    render :inline=>"<script type='text/javascript'>var p = window.location.href.split('#');window.location.href = '/logins/get_info?'+p.length>1 ? p[1] : '';</script>"
+   render :inline=>"<script type='text/javascript'>var p = window.location.href.split('#');var pr = p.length>1 ? p[1] : '';window.location.href = '/logins/get_info?'+pr;</script>"
   end
 end
