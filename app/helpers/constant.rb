58a78487c0e4467de80dfec4ce75ae409cc2b2fb
lifang <santhe_2004@126.com>
2012-07-30 09:31:15 +0800

    update detial

--- app/helpers/constant.rb
+++ app/helpers/constant.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 module Constant
   SERVER_URL = "http://kaoyan.gankao.co"
-  MAIN_URL = "http://apps.renren.com/wanteight"
+  MAIN_URL = "http://apps.renren.com/kaoyantgl"
 
   APP_ID="196777"
   RENREN_API_SECRET="79fb9cf508dd4751a1c3260ab57b43be"
