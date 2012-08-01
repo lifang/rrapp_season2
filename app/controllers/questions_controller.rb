14723e86795cd3bb4f42fd5096300541459ed11a
xuya <xuyae573@foxmail.com>
2012-08-01 16:42:56 +0800

    modfity quesitons and result html

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,23 +8,22 @@ class QuestionsController < ApplicationController
 
   
   def result
-    @score = [rand(25) + 45, rand(30) + 50, rand(35) + 100]
-    @is_yuwen = params[:is_yuwen].to_i
-    message="我刚刚对自己考研成绩进行了预测，竟然有英语 #{@score[0]}，政治 #{@score[1]}"
-    if @is_yuwen == 1
-      @total_score = @score[0] + @score[1] + 117
-    else
-      message +=",数学 #{@score[2]}"
-      @total_score = @score[0] + @score[1] + @score[2]
-    end
-    if @total_score>=195 && @total_score <=220
-      message +=",还不赖，不过还得继续努力哦。"
-    elsif @total_score>=221 && @total_score <250
-      message +=",有戏！有戏！！"
-    elsif @total_score>=250 && @total_score <270
-      message +=",希望那是相当的大，O(∩_∩)O哈哈~，继续加油！！"
-    elsif @total_score>=270 && @total_score <285
-      message +=",十拿九稳咯，不过不能掉以轻心哦。"
+    @sum = params[:sum].to_i
+    if @sum == 4||@sum==5
+      @score= rand(9)+40
+      @message ="我刚刚对自己考研英语分数进行了预测，只有#{@score}分呀，我要继续加油努力，希望还是很大嘛~O(∩_∩)O~"
+    elsif @sum==6 ||@sum==7
+      @score=rand(9)+50
+      @message="我刚刚对自己考研英语分数进行了预测，只有#{@score}分呀，希望就在前方，我去图书馆K书啦~O(∩_∩)O~"
+    elsif @sum==8 || @sum==9
+      @score=rand(9)+60
+      @message="我刚刚对自己考研英语分数进行了预测，竟然是#{@score}分，还不错嘛~O(∩_∩)O~哈哈~，得瑟3秒钟，然后看书！！"
+    elsif @sum==10
+      @score=rand(9)+70
+      @message="我刚刚对自己考研英语分数进行了预测，竟然是#{@score}分，太棒了~O(∩_∩)O我是报北大呢，还是报清华呢！！"
+    elsif @sum==11|| @sum==12
+      @score=rand(9)+80
+      @message="我刚刚对自己考研英语分数进行了预测，竟然有#{@score}分，我干脆去办考研英语辅导班吧。嘿嘿！"
     end
     if params[:share].to_i==1
       renren_send_message(session[:user_info]["access_token"],message)
